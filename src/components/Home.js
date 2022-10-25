71bbb16268c95d4a1474b0f9b8e549df68ebb739
Thomas Fouquert <tfouquert@gmail.com>
2022-10-25 15:55:17 +0200

    add progress bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import Result from './Result'
 import { FetchData } from '../Api/FetchData'
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const Home = ({datas, setDatas}) => {
 
   const [categories, setCategories] = useState([])
   const [loadCategories, setLoadCategories] = useState(true)
+  const [scroll, setScroll] = useState(0)
 
   useEffect(() => {
 
@@ -34,6 +36,19 @@ const Home = ({datas, setDatas}) => {
   }, [loadCategories, categories])
 
 
+  const handleScroll = (e) => {
+
+     const main = e.target.scrollWidth
+     let left = e.target.scrollLeft
+
+     let level = (left /main) * 100
+
+     setScroll(level)
+
+  }
+
+
+
   const handleClick =  async (category) => {
 
 
@@ -49,7 +64,7 @@ const Home = ({datas, setDatas}) => {
     <>
 
 
-      <div className="cards img-slider__container-1 mt-5 ">
+      <div className="cards img-slider__container-1 mt-5 mx-3" onScroll={handleScroll}>
 
         {
           categories?.map((category, index) => {
@@ -64,12 +79,17 @@ const Home = ({datas, setDatas}) => {
                     >
                     <h3 className='text-white'>{category}</h3>
                   </div>
+
+
               )
           })
         }
 
       </div>
 
+
+
+      <ProgressBar animated now={scroll} />
 
       <Result datas={datas} />
 
